d919c6951b4e6b04c2b64afeb00d6bceef995e83
Richard Chalk <barmybrit@gmail.com>
2024-10-31 06:54:46 +0100

    Add project files.

---
+++ LibraryAndServices/Program.cs
@@ -0,0 +1,29 @@
+﻿using MyClassLibrary;
+
+namespace LibraryAndServices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstCustomer = new Customer(1, "Richard", "rich@email.com");
+            // Console.WriteLine(firstCustomer.Name);
+
+            var customers = new List<Customer>
+            {
+                new Customer(1, "Richard", "rich@email.com"),
+                new Customer(2, "Stefan", "stefan@email.com"),
+                new Customer(3, "Linda", "linda@email.com"),
+                new Customer(4, "Alicia", "alicia@email.com"),
+            };
+
+            foreach (var c in customers)
+            {
+                Console.WriteLine($"id: {c.CustomerId}, Name: {c.Name}, Email: {c.Email}");
+            }
+
+
+
+        }
+    }
+}
---
+++ MyClassLibrary/Customer.cs
@@ -0,0 +1,21 @@
+﻿namespace MyClassLibrary
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string? Address { get; set; }
+        public int? PostNumber { get; set; }
+        public string? City { get; set; }
+        public bool? IsVIP { get; set; }
+
+        public Customer(int id, string name, string email)
+        {
+            CustomerId = id;
+            Name = name;
+            Email = email;
+        }
+
+    }
+}
